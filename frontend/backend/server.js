f59394fa455fc6de7c0c558930e0977985fbb31b
imanav10 <manavrj.07@gmail.com>
2024-09-03 08:54:16 +0530

    yes

--- frontend/backend/server.js
+++ frontend/backend/server.js
@@ -30,7 +30,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
